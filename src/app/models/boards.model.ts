2853e48f3cb221119591c9db201d2172f893c33c
Deiby Daniela Rique Porras <drique@asesoftware.com>
2023-03-15 17:39:45 -0500

    video 5 terminado

---
+++ src/app/models/boards.model.ts
@@ -0,0 +1,9 @@
+import { Colors } from "./colors.model";
+import { User } from "./user.model";
+
+export interface Board{
+    id: string;
+    title: string;
+    backgroundColor: Colors;
+    members: User[];
+}
